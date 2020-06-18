3226356cee8de21435632344a123af4c574219e0
Jo Anna Mollman <joanna.mollman@gmail.com>
2020-06-18 08:39:20 -0700

    add helpers

--- copyspecial.py
+++ copyspecial.py
@@ -7,7 +7,7 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 
 # give credits
-__author__ = "Jo Anna Mollman (jodi08)"
+__author__ = "Jo Anna Mollman (jodi08) with help from Amanda, Nikal, and Albina"
 
 import re
 import os
